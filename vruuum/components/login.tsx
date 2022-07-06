7557faf3c88966f9173900c8a8ddc5a20d0f4f86
Ellian Carlos <17656878+EllianCarlos@users.noreply.github.com>
2022-07-06 00:46:46 -0300

    feat: faz login, mas não redireciona

--- vruuum/components/login.tsx
+++ vruuum/components/login.tsx
@@ -1,14 +1,49 @@
 import { FC } from "react";
 import styles from "../styles/Login.module.css";
+import { useCookies } from "react-cookie";
 
 const Login: FC<any> = () => {
+  const [cookie, setCookie] = useCookies(["user"]);
+  // Handles the submit event on form submit.
+  const handleSubmit = async (event: any) => {
+    event.preventDefault();
+    const data = {
+      login: event.target.login.value,
+      password: event.target.password.value,
+    };
+    const JSONdata = JSON.stringify(data);
+    const endpoint = "/api/login";
+
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSONdata,
+    };
+
+    const response = await fetch(endpoint, options);
+    if (!response.ok) {
+      return;
+    }
+    const result = await response.json();
+
+    // @ts-ignore
+    setCookie("user_type", result.type, {
+      path: "/",
+      maxAge: 3600,
+      sameSite: false,
+    });
+
+  };
+
   return (
     <center>
       <br />
       <main className={styles.container}>
-        <form action="">
-          <input type="text" name="Login" id="Login" placeholder="Login" />
-          <input type="text" name="Senha" id="Senha" placeholder="Senha" />
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="login" id="Login" placeholder="Login" />
+          <input type="text" name="password" id="Senha" placeholder="Senha" />
           <br />
           <button type="submit">Enviar</button>
         </form>
--- vruuum/pages/api/login.ts
+++ vruuum/pages/api/login.ts
@@ -1,30 +1,42 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import prisma from "@/lib/prisma";
 import { PrismaClient } from "@prisma/client";
-import md5 from 'md5';
+import md5 from "md5";
 
-// Exemplo de como adicionar endpoint na api
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ): Promise<void> {
   if (req.method === "POST") {
     try {
+      console.log(req.body);
       const login = req.body.login;
       const password = req.body.password;
+      console.log(login);
+      console.log(password);
 
       let prismaClient = prisma as PrismaClient;
-      const [userPassword, tipo, userid]: any = await prismaClient.$queryRaw`SELECT password, tipo, userid FROM login WHERE login = ${login}`;
+      const query: any[] =
+        await prismaClient.$queryRaw`SELECT password, tipo, userid FROM users WHERE login = ${login}`;
 
-      if (!!userPassword || !!tipo) {
-        return res.status(503).json({ msg: "Login não reconhecido" });
+      if (query.length > 1) {
+        return res.status(503).json({ msg: "Login não reconhecido 1" });
+      }
+
+      const queryData = query[0];
+      const userPassword = queryData.password;
+      const userId = queryData.userid;
+      const tipo = queryData.tipo;
+
+      if (!userPassword || userId === null || userId === undefined) {
+        return res.status(503).json({ msg: "Login não reconhecido 1" });
       }
 
       if (md5(password) !== userPassword) {
-        return res.status(503).json({ msg: "Login não reconhecido" });
+        return res.status(503).json({ msg: "Login não reconhecido 2" });
       }
 
-      return res.status(200).json({ tipo });
+      return res.status(200).json({ type: tipo, userId: userId });
     } catch (err) {
       console.error(err);
       return res.status(500).json({ msg: "Something went wrong" });
